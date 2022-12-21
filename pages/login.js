e9e3788713a07fa32b4d6511b2700d78e6da715c
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2022-12-21 14:17:23 +0500

    fixed

--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-
-
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
 
 const Login = () => {
   const router = useRouter();
-  
+  const [inputValue, setInputValue] = useState("");
+  const [validationState, setValidationState] = useState(null);
+
   //  handle login
   const handleLogin = () => {
-    router.push("/")
-    localStorage.setItem("isLogged", true)
-    // window.location.reload(true) 
+    router.push("/");
+    localStorage.setItem("isLogged", true); 
+    // window.location.reload(true)
+  };
+
+  function handleChange(event) {
+    setInputValue(event.target.value);
+    setValidationState(event.target.value === "");
   }
+
+
 
   return (
     <div>
-        <div className="login">
+      <div className="login">
         <div className="login_inner">
-          <h2>Login</h2> 
+          <h2>Login</h2>
           <div className="login_inputs">
-          <input type="text" placeholder='Email' />
-          <input type="pwd" placeholder='Password' />
-          <button onClick={ handleLogin } >Login</button>  
+            <input
+              type="text"
+              value={inputValue}
+              style={{
+                backgroundColor: validationState ? validationState === true ? "red" : "green" : validationState === false && "green" ,
+              }}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              placeholder="Email"
+            />
+            {/* <input type="pwd" value={ password } className={ passwordReq && password ? "required" : "noRequired" } onChange={(e) => setPassword(e?.target?.value) } placeholder='Password' /> */}
+            <button onClick={handleLogin}>Login</button>
           </div>
         </div>
-        </div>      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
